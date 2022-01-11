4556310712d35a4f6e307e212dfdb39b54921d75
samuelthelaus <samuel.thelaus@gmail.com>
2022-01-11 11:20:16 +0000

    Cleaned up notebook and added viz

---
+++ viz and interpretable.R
@@ -0,0 +1,9 @@
+
+# Visualization and small logistic regression for interpretability
+
+suppressMessages({
+  library(tidyverse)
+})
+
+the_data_full <- read.csv('C:/Users/samue/Documents/to_r_tdab_full.csv')
+the_data_super <- read.csv('C:/Users/samue/Documents/to_r_tdab_super.csv')
